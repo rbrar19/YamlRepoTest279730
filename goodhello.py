121afd15b2b386505af505985032fa0642a3e88c
Rubalpreet Kaur Brar <rbrar19@example.com>
2026-01-20 12:35:38 -0600

    added new files

---
+++ goodhello.py
@@ -0,0 +1,11 @@
+def hello():
+    """
+    Prints a simplest greeting.
+    """
+    print("Hello, DevOps learner!")
+
+def bye():
+    """
+    Prints a goodbye greeting.
+    """
+    print("Bye, DevOps learner!")
---
+++ test_main.py
@@ -0,0 +1,7 @@
+from goodhello import hello, bye
+
+def test_hello_return_none():
+    assert hello() is none
+
+def test_bye_return_none():
+    assert bye() is none
